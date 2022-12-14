92966ffc43486530002c42ec38df8f038124b797
Tuğkan Meral <tugkan.mrl@gmail.com>
2022-12-14 11:09:24 +0300

    improve paging component

--- src/Web/the-host-react-js/src/components/Paging.js
+++ src/Web/the-host-react-js/src/components/Paging.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 function Paging(props) {
     const [activePage, setActivePage] = useState(props.activePage);
+    const [prevButtonVisible, setPrevButtonVisibility] = useState(false);
+    const [nextButtonVisible, setNextButtonVisibility] = useState(true);
 
     let tempVisiblePage = []
     for (let index = 0; index < props.pageCount; index++) {
@@ -9,12 +11,47 @@ function Paging(props) {
         if (index > 2)
             break
     }
-    
+
     const [visiblePages, setVisiblePages] = useState(tempVisiblePage);
 
     function changePage(_page) {
+        if (_page == activePage)
+            return
+            
         setActivePage(_page)
         props.onClick(_page)
+
+        setDirectionButtonVisibility(_page);
+    }
+
+    function setDirectionButtonVisibility(_activePage) {
+        if (_activePage <= 1) {
+            setPrevButtonVisibility(false)
+        }
+        else {
+            setPrevButtonVisibility(true)
+        }
+
+        let any = visiblePages.some(x => x == (_activePage + 1))
+        if (any)
+            setNextButtonVisibility(true)
+        else
+            setNextButtonVisibility(false)
+
+    }
+
+    function nextPage() {
+        let any = visiblePages.some(x => x == (activePage + 1))
+        console.log(any)
+        if (any)
+            changePage(activePage + 1)
+    }
+
+    function previousPage() {
+        if (activePage <= 1)
+            return
+
+        changePage(activePage - 1)
     }
 
     function GetVisiblePages() {
@@ -33,16 +70,22 @@ function Paging(props) {
     return (
         <div className="center-row">
             <ul className="pagination">
-                <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Previous">
+                <li className={prevButtonVisible ? "page-item" : "page-item disabled"}>
+                    <button
+                        className="page-link"
+                        onClick={previousPage}
+                        aria-label="Previous">
                         <span aria-hidden="true">&laquo;</span>
-                    </a>
+                    </button>
                 </li>
                 <GetVisiblePages />
-                <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Next">
+                <li className={nextButtonVisible ? "page-item" : "page-item disabled"}>
+                    <button
+                        className="page-link"
+                        onClick={nextPage}
+                        aria-label="Next">
                         <span aria-hidden="true">&raquo;</span>
-                    </a>
+                    </button>
                 </li>
             </ul>
         </div>
