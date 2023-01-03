38c5bed6d0ecfa08148e4b551233707a04276d6c
Tuğkan Meral <tugkan.mrl@gmail.com>
2023-01-04 01:20:10 +0300

    update Display service

--- src/Services/Monitoring/Business/Worker.cs
+++ src/Services/Monitoring/Business/Worker.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -20,8 +21,8 @@ public class Worker : IHostedService, IDisposable
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("in StartAsync method");
-        
-        _timer = new Timer(Dowork, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+        _timer = new Timer(Dowork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
 
         return Task.CompletedTask;
     }
@@ -29,16 +30,36 @@ public class Worker : IHostedService, IDisposable
     public Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Timed Hosted Service is stopping. Count: {executionCount}", executionCount);
-        
+
         _timer?.Change(Timeout.Infinite, 0);
 
         return Task.CompletedTask;
     }
 
-    private void Dowork(object? state)
+    private async void Dowork(object? state)
     {
+        using var client = new HttpClient();
+        var result = await client.GetAsync("https://yesno.wtf/api");
+        if (result.IsSuccessStatusCode)
+        {
+            var responseStr = await result.Content.ReadAsStringAsync();
+            _logger.LogInformation(responseStr);
+            var response = JsonSerializer.Deserialize<Root>(responseStr, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            _logger.LogInformation($"Answer is {response?.Answer}");
+        }
+
         var count = Interlocked.Increment(ref executionCount);
 
         _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
     }
+}
+
+public class Root
+{
+    public string Answer { get; set; } = null!;
+    public bool Forced { get; set; }
+    public string Image { get; set; } = null!;
 }
