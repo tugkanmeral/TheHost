5b7f646f7f2cec855d994be277d030e800a8b0dc
Tuğkan Meral <tugkan.mrl@gmail.com>
2023-01-05 01:49:11 +0300

    improved monitoring service

--- src/Services/Monitoring/Business/Worker.cs
+++ src/Services/Monitoring/Business/Worker.cs
@@ -4,9 +4,15 @@ using Microsoft.Extensions.Logging;
 
 public class Worker : IHostedService, IDisposable
 {
-    private int executionCount = 0;
     private readonly ILogger<Worker> _logger;
     private Timer? _timer;
+    private readonly HashSet<ServiceInfo> SERVICES = new HashSet<ServiceInfo>()
+    {
+        new ServiceInfo("Note", "http://note:8080/healthCheck"),
+        new ServiceInfo("User", "http://user:8080/healthCheck"),
+        new ServiceInfo("Password", "http://password:8080/healthCheck"),
+        new ServiceInfo("Tool", "http://tool:8080/healthCheck")
+    };
 
     public Worker(ILogger<Worker> logger)
     {
@@ -20,17 +26,13 @@ public class Worker : IHostedService, IDisposable
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("in StartAsync method");
-
-        _timer = new Timer(Dowork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+        _timer = new Timer(Dowork, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
 
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Timed Hosted Service is stopping. Count: {executionCount}", executionCount);
-
         _timer?.Change(Timeout.Infinite, 0);
 
         return Task.CompletedTask;
@@ -39,27 +41,27 @@ public class Worker : IHostedService, IDisposable
     private async void Dowork(object? state)
     {
         using var client = new HttpClient();
-        var result = await client.GetAsync("https://yesno.wtf/api");
-        if (result.IsSuccessStatusCode)
+        foreach (var service in SERVICES)
         {
-            var responseStr = await result.Content.ReadAsStringAsync();
-            _logger.LogInformation(responseStr);
-            var response = JsonSerializer.Deserialize<Root>(responseStr, new JsonSerializerOptions()
+            try
             {
-                PropertyNameCaseInsensitive = true
-            });
-            _logger.LogInformation($"Answer is {response?.Answer}");
+                var result = await client.GetAsync($"{service.HealthCheckUrl}");
+                if (result.IsSuccessStatusCode)
+                {
+                    var responseStr = await result.Content.ReadAsStringAsync();
+                    _logger.LogInformation(service.Name + " : " + responseStr);
+                }
+                else
+                {
+                    _logger.LogWarning(service.Name + " : " + result.StatusCode.ToString());
+                }
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(service.Name + " : " + ex.ToString());
+            }
         }
-
-        var count = Interlocked.Increment(ref executionCount);
-
-        _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
     }
 }
 
-public class Root
-{
-    public string Answer { get; set; } = null!;
-    public bool Forced { get; set; }
-    public string Image { get; set; } = null!;
-}
+public record ServiceInfo(string Name, string HealthCheckUrl);
