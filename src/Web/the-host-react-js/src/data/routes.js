2beeb844b3a78c9af8402d54d6598bca2b3f1b65
Tuğkan Meral <tugkan.mrl@gmail.com>
2023-01-28 21:08:50 +0300

    set routes according to api gateway

--- src/Web/the-host-react-js/src/data/routes.js
+++ src/Web/the-host-react-js/src/data/routes.js
@@ -1,21 +1,21 @@
 // Login
-export const GET_TOKEN_URL = "http://localhost:5001/api/auth/getToken"
+export const GET_TOKEN_URL = "http://localhost:5005/auth/getToken" //"http://localhost:5001/api/auth/getToken"
 
 // Password
-export const GET_PASSWORDS_URL = "http://localhost:5002/api/passwords"
-export const GET_PASSWORD_URL = "http://localhost:5002/api/passwords/"
-export const DELETE_PASSWORD_URL = "http://localhost:5002/api/passwords/"
-export const POST_PASSWORD_URL = "http://localhost:5002/api/passwords"
-export const PUT_PASSWORD_URL = "http://localhost:5002/api/passwords/"
+export const GET_PASSWORDS_URL = "http://localhost:5005/passwords" // "http://localhost:5002/api/passwords"
+export const GET_PASSWORD_URL = "http://localhost:5005/passwords/" // "http://localhost:5002/api/passwords/"
+export const DELETE_PASSWORD_URL = "http://localhost:5005/passwords/" // "http://localhost:5002/api/passwords/"
+export const POST_PASSWORD_URL = "http://localhost:5005/passwords" // "http://localhost:5002/api/passwords"
+export const PUT_PASSWORD_URL = "http://localhost:5005/passwords/" // "http://localhost:5002/api/passwords/"
 
 // Note
-export const GET_NOTES_URL = "http://localhost:5003/api/notes"
-export const GET_NOTE_URL = "http://localhost:5003/api/notes/"
-export const INSERT_NOTE_URL = "http://localhost:5003/api/notes"
-export const UPDATE_NOTE_URL = "http://localhost:5003/api/notes/" 
-export const DELETE_NOTE_URL = "http://localhost:5003/api/notes/" 
+export const GET_NOTES_URL = "http://localhost:5005/notes" // "http://localhost:5003/api/notes"
+export const GET_NOTE_URL = "http://localhost:5005/notes/" // "http://localhost:5003/api/notes/"
+export const INSERT_NOTE_URL = "http://localhost:5005/notes" // "http://localhost:5003/api/notes"
+export const UPDATE_NOTE_URL = "http://localhost:5005/notes/"  // "http://localhost:5003/api/notes/" 
+export const DELETE_NOTE_URL = "http://localhost:5005/notes/" // "http://localhost:5003/api/notes/" 
 
 // User
-export const USER_REGISTER_URL = "http://localhost:5004/api/user"
+export const USER_REGISTER_URL = "http://localhost:5005/user" // "http://localhost:5004/api/user"
 
 
