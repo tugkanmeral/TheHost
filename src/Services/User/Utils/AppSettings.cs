0ac54cf0ef441e9caa0dda773977449fda91b6ba
Tuğkan Meral <tugkan.mrl@gmail.com>
2023-03-09 14:31:21 +0300

    update gitignore

---
+++ src/Services/User/Utils/AppSettings.cs
@@ -0,0 +1,13 @@
+public class AppSettings
+{
+    public string IV { get; set; } = String.Empty;
+    public string MongoUri { get; set; } = String.Empty;
+    public PasswordSalt Salt { get; set; } = new();
+    public string AuthSecretKey { get; set; } = String.Empty;
+}
+
+public class PasswordSalt
+{
+    public string Value { get; set; } = String.Empty;
+    public int StartIndex { get; set; }
+}
